39bf5979db3a885e7e344fbf56d05008a183d407
Masaab Gul <engmasaabgul@gmail.com>
2023-06-22 19:12:59 +0500

    Added the from date validation in add appointment

--- src/validations/appointment.ts
+++ src/validations/appointment.ts
@@ -1,13 +1,26 @@
-import Joi from "joi";
+import Joi, { CustomHelpers } from "joi";
 
+const validateFromDate = (value: Date, helpers: CustomHelpers) => {
+  const currentDate = new Date();
+  const expectedDate = new Date(currentDate.getTime() + 60 * 60 * 1000);
+
+  if (value.getTime() === expectedDate.getTime()) {
+    return value;
+  }
+
+  return helpers.error("any.invalid");
+};
 export const AddAppointmentSchema = Joi.object({
   time: Joi.date().default(Date.now),
   status: Joi.string().default("Waiting"),
-  fromTime: Joi.date().iso().required().messages({
-    "date.base": "The from time field must be a valid date.",
-    "date.format": "The from time field must be in ISO format (YYYY-MM-DD).",
-    "date.required": "The from time field is required.",
-  }),
+  fromTime: Joi.date()
+    .custom(validateFromDate, "From Date validator")
+    .required()
+    .messages({
+      "date.base": "The from time field must be a valid date.",
+      "date.format": "The from time field must be in ISO format (YYYY-MM-DD).",
+      "date.required": "The from time field is required.",
+    }),
   toTime: Joi.date().iso().required().messages({
     "date.base": "The To time field must be a valid date.",
     "date.format": "The To time field must be in ISO format (YYYY-MM-DD).",
