3074e65fd9d92a390e2da059f650fa2106922bad
Masaab Gul <engmasaabgul@gmail.com>
2023-06-24 15:05:19 +0500

    Fixed date validation

--- src/validations/appointment.ts
+++ src/validations/appointment.ts
@@ -1,29 +1,14 @@
 import Joi, { CustomHelpers } from "joi";
 
-const validateFromDate = (value: Date, helpers: CustomHelpers) => {
-  const currentDate = new Date();
-  const expectedDate = new Date(currentDate.getTime() + 60 * 60 * 1000);
-
-  if (value.getTime() === expectedDate.getTime()) {
-    return value;
-  }
-
-  return helpers.error("any.invalid");
-};
 export const AddAppointmentSchema = Joi.object({
   time: Joi.date().default(Date.now),
   status: Joi.string().default("Waiting"),
-  fromTime: Joi.date()
-    .custom(validateFromDate, "From Date validator")
-    .required()
-    .messages({
-      "date.base": "The from time field must be a valid date.",
-      "date.format": "The from time field must be in ISO format (YYYY-MM-DD).",
-      "date.required": "The from time field is required.",
-    }),
-  toTime: Joi.date().iso().required().messages({
+  fromTime: Joi.date().required().messages({
+    "date.base": "The from time field must be a valid date.",
+    "date.required": "The from time field is required.",
+  }),
+  toTime: Joi.date().required().messages({
     "date.base": "The To time field must be a valid date.",
-    "date.format": "The To time field must be in ISO format (YYYY-MM-DD).",
     "date.required": "The To time field is required.",
   }),
   opdId: Joi.string().empty().required().messages({
